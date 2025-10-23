5869257b22085a73f71d8f5cbb14487aa1e96249
sr20karol <karol.m99@interia.pl>
2025-10-23 13:29:34 +0200

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Simulator;
+
+public class Animals
+{
+    public required string Description { get; init; }
+    public uint Size { get; set; } = 3;
+    public string Info => $"{Description} <{Size}>";
+}
---
+++ Simulator/Creature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Simulator;
+
+internal class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public Creature()
+    {
+    }
+
+    public Creature(string name, int level = 1)
+    {
+        this.Name = name;
+        this.Level = level;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
+    public string Info => $"{Name} [{Level}]";
+}
